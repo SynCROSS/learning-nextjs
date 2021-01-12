b220f368f974f9114636e2ba89d82384546dcc59
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-01-12 22:37:45 +0900

    styled-jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,6 +2,13 @@ import Layout from '../components/Layout';
 
 const About = () => (
   <Layout>
+    <style jsx>
+      {`
+        h1 {
+          color: #dc143c;
+        }
+      `}
+    </style>
     <h1>About</h1>
     <p>The inside of the Link Component must be a component or element.</p>
   </Layout>
