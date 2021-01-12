b0cacf5729371544aec9317ea8b6fbe620b838b5
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-01-12 20:17:02 +0900

    Next.js is awesome

---
+++ pages/index.jsx
@@ -0,0 +1,39 @@
+const Index = () => (
+  <div id="root">
+    <h1>Hello, Next.js</h1>
+    <p>
+      We don't need{' '}
+      <span
+        style={{
+          fontFamily:
+            'SFMono-Regular, Consolas, Liberation Mono, Menlo, Courier, monospace',
+          lineHeight: 'normal',
+          background: 'rgba(135,131,120,0.15)',
+          color: '#EB5757',
+          borderRadius: '3px',
+          fontSize: '85%',
+          padding: '0.2em 0.4em',
+        }}
+        data-token-index="0">
+        CRA
+      </span>{' '}
+      and{' '}
+      <span
+        style={{
+          fontFamily:
+            'SFMono-Regular, Consolas, Liberation Mono, Menlo, Courier, monospace',
+          lineHeight: 'normal',
+          background: 'rgba(135,131,120,0.15)',
+          color: '#EB5757',
+          borderRadius: '3px',
+          fontSize: '85%',
+          padding: '0.2em 0.4em',
+        }}
+        data-token-index="0">
+        import React from 'react';
+      </span>
+    </p>
+  </div>
+);
+
+export default Index;
